82e7f7bf8948c2ac51ed8fd52d3ccd5417cc871f
Wander Júnior Cruz <wanderjrcruz96@gmail.com>
2021-04-14 17:23:52 -0300

    Create Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Search</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
